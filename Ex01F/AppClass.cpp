095ad1f7db436f168a9307f823cdd8480cd0335c
MikeCampito <Mikecampito@gmail.com>
2018-03-22 03:17:29 -0700

    Midterm 2 Done

---
+++ Ex01F/AppClass.cpp
@@ -0,0 +1,113 @@
+#include "AppClass.h"
+using namespace Simplex;
+void Application::Display(void)
+{
+#pragma region DOES NOT NEED CHANGES
+	// Clear the screen
+	ClearScreen();
+
+	//Get a timer
+	static float fTimer = 0;	//store the new timer
+	static uint uClock = m_pSystem->GenClock(); //generate a new clock for that timer
+	fTimer += m_pSystem->GetDeltaTime(uClock); //get the delta time for that timer
+#pragma endregion
+	//Your code goes here ---------------------------------------------------------
+	
+	m_sProgrammer = "Michael Campito - mjc1264@g.rit.edu"; //Replace with your name and email
+	vector3 v3CurrentPos; //Initialize this variable accordingly
+	
+						  //If at the end of the list
+	if (count + 1 == m_v3StopList.size()) {
+		v3CurrentPos = glm::lerp(m_v3StopList[count], m_v3StopList[0], fTimer);  //Lerp back to the start
+
+																				 //If it's been more than one second
+		if (fTimer >= 1) {
+			count = 0;  //Resets the count to zero
+			fTimer = 0;  //Resets the timer to zero
+		}
+	}
+	//If anywhere else on the list
+	else {
+		v3CurrentPos = glm::lerp(m_v3StopList[count], m_v3StopList[count + 1], fTimer);  //Lerps to the next point in the list
+
+																						 //If it's been more than one second
+		if (fTimer >= 1) {
+			count++;  //Increments the count by one
+			fTimer = 0;  //Resets the timer to zero
+		}
+	}
+
+	matrix4 m4Model = glm::translate(IDENTITY_M4, v3CurrentPos) * ToMatrix4(m_qArcBall);
+	
+	//---------------------------------------------------------
+#pragma region DOES NOT NEED CHANGES
+	matrix4 m4View = m_pCameraMngr->GetViewMatrix(); //view Matrix
+	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix(); //Projection Matrix
+
+	// render the object
+	m_pMesh->Render(m4Projection, m4View, m4Model);
+
+	// draw stops to know we are within stops
+	for (uint i = 0; i < m_v3StopList.size(); ++i)
+	{
+		m_pMeshMngr->AddSphereToRenderList(glm::translate(m_v3StopList[i]) * glm::scale(vector3(0.10f)), C_RED);
+	}
+		
+	// draw a skybox
+	m_pMeshMngr->AddSkyboxToRenderList();
+
+	//render list call
+	m_uRenderCallCount = m_pMeshMngr->Render();
+
+	//clear the render list
+	m_pMeshMngr->ClearRenderList();
+
+	//draw gui
+	DrawGUI();
+
+	//end the current frame (internally swaps the front and back buffers)
+	m_pWindow->display();
+#pragma endregion
+}
+#pragma region DOES NOT NEED CHANGES
+void Application::InitVariables(void)
+{
+	//Set the position and target of the camera
+	//(I'm at [0,0,10], looking at [0,0,0] and up is the positive Y axis)
+	m_pCameraMngr->SetPositionTargetAndUp(AXIS_Z * 15.0f, ZERO_V3, AXIS_Y);
+
+	//initializing the model
+	m_pMesh = new Simplex::MyMesh();
+
+	//Generate a primitive
+	m_pMesh->GenerateCone(1.5f, 2.0f, 5, C_YELLOW);
+
+	//Check if stops is empty if it is add some points to it
+	if (m_v3StopList.size() == 0)
+	{
+		m_v3StopList.push_back(vector3());
+		m_v3StopList.push_back(vector3(2.5f, 0.0f, 0.0f));
+		m_v3StopList.push_back(vector3(-1.0f, 2.5f, 0.0f));
+		m_v3StopList.push_back(vector3(-2.5f));
+	}
+}
+void Application::Update(void)
+{
+	//Update the system so it knows how much time has passed since the last call
+	m_pSystem->Update();
+
+	//Is the arcball active?
+	ArcBall();
+
+	//Is the first person camera active?
+	CameraRotation();
+}
+void Application::Release(void)
+{
+	//release model
+	SafeDelete(m_pMesh);
+
+	//release GUI
+	ShutdownGUI();
+}
+#pragma endregion
